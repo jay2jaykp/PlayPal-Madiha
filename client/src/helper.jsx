08b9b8febb64f1d66dcebcc69c69cd77e11d4da8
Madiha Umair <89612715+Madiha-umair@users.noreply.github.com>
2023-06-26 20:17:01 -0400

    Update helper.jsx

--- client/src/helper.jsx
+++ client/src/helper.jsx
@@ -1,7 +1,7 @@
 //export const REACT_APP_BASE_URL= "http://localhost:8000";
 //const BASE_URL="https://playpalbackend.onrender.com/";
 
-const BASE_URL = 'https://playpalbackend.onrender.com';
+const BASE_URL = 'https://playpalbackend-production.up.railway.app/';
 
 //const BASE_URL="http://localhost:8000";
 
